3b8552fdb2ce3a38e998fc16f8b44cd787a484cb
Elimarie <iglitzi@fullsail.edu>
2015-10-30 11:46:26 -0400

    python
    conditional and collections

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -4,7 +4,7 @@ Doc strings
 Multiples comentarios
 http:legacy.pythong.org//pep-0008/
 '''
-
+'''
 first_name = "Kermit"
 last_name = "De Frog"
 
@@ -15,22 +15,32 @@ print first_name
 #print response
 
 #print "Hello there, ", response
-
-'''simple math'''
+'''
+'''simple math
 birth_year = 1975
 current_year = 2015
 
 age = current_year - birth_year
-
+'''
 #print age
 
-'''assignment operators increment decrement suma resta'''
+'''
+comments, variables and printing in python
+assignment operators increment decrement suma resta
 
 
 print "You are " +  str(age) + " years old"
 
+conditional and collections
+'''
+budget = 50
 
+if budget > 100:
+    brand = 'Nike'
+    print 'Yay! we can buy cool ' + brand + ' shoes'
 
+else:
+    print 'No cool shoes for me'
 
 
 
