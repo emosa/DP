fb3d2fff7b72915b46fefa22ab9bdb0d93f1e87e
Elimarie <iglitzi@fullsail.edu>
2015-10-30 11:22:10 -0400

    python
    continuation

--- Intro to Python/main.py
+++ Intro to Python/main.py
@@ -1,1 +1,47 @@
-print "hello wold"
+#line comment -solo un comentario
+'''
+Doc strings
+Multiples comentarios
+http:legacy.pythong.org//pep-0008/
+'''
+
+first_name = "Kermit"
+last_name = "De Frog"
+
+print first_name
+
+#response = raw_input("Enter your name   ")
+
+#print response
+
+#print "Hello there, ", response
+
+'''simple math'''
+birth_year = 1975
+current_year = 2015
+
+age = current_year - birth_year
+
+#print age
+
+'''assignment operators increment decrement suma resta'''
+
+
+print "You are " +  str(age) + " years old"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
