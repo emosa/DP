96f941500e247b34b0fba724667d2d6fba191e22
Elimarie <iglitzi@fullsail.edu>
2015-11-08 13:38:00 -0500

    simple form
    this will require student info

--- simple-login/main.py
+++ simple-login/main.py
@@ -12,7 +12,7 @@ import webapp2   #used the webapp2 library
 
 class MainHandler(webapp2.RequestHandler): #declarin a class
     def get(self): # FUNCTION STARTS EVERYTHING
-
+            #Document heading
         page_head = '''
                     <!DOCTYPE html>
                     <html>
@@ -22,6 +22,28 @@ class MainHandler(webapp2.RequestHandler): #declarin a class
                     </head>
                     <body>
                     '''
+            #This will require student information
+        page_body = '''
+                    <form method = "GET" >
+                    Name: <br><input type="text" name="name" /><br><br>
+                    Email: <br><input type="text" name="email" /><br><br>
+                    Age: <br> <input type="text" name="age" /><br><br>
+                    Sex: <br> <input type="radio" name="sex" value="male">Male <input type="radio" name="sex" value="Female"<br><br>
+                    Gpa:
+                        <br> select name="gpa">
+                          <option value="From 5.00 to 4.00">From 5.00 to 4.00</option>
+                          <option value="From 4.00 to 3.00">From 4.00 to 3.00</option>
+                          <option value="From 3.00 to 2.00">From 3.00 to 2.00</option>
+                          <option value="From 2.00 to 1.00">From 2.00 to 1.00</option>
+                    <input type="submit" value="Submit">'''
+        page_close = '''
+                     </form>
+                     </body>
+                     </html>'''
+
+
+
+
 
 
 
