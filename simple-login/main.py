4ade28fc4732ededa0e1e56712ec97998bcc1269
Elimarie <iglitzi@fullsail.edu>
2015-11-08 16:28:37 -0500

    now form
    working

--- simple-login/main.py
+++ simple-login/main.py
@@ -1,5 +1,3 @@
-
-
 '''
 ELIMARIE MORALES SANTIAGO
 FULL SAIL UNIVERSITY
@@ -8,62 +6,57 @@ Simple Form
 
 '''
 
-import webapp2   #used the webapp2 library
+import webapp2  # used the webapp2 library
 
-class MainHandler(webapp2.RequestHandler): #declarin a class
-    def get(self): # FUNCTION STARTS EVERYTHING
-            #Document heading
-        page_head = '''
-                    <!DOCTYPE html>
-                    <html>
-                    <head>
-                    <meta charset="UTF-8">
-                    <title>Patient Information Form</title>
-                    </head>
-                    <body>
+
+class MainHandler(webapp2.RequestHandler):  # declaring a class
+    def get(self):  # FUNCTION STARTS EVERYTHING
+        # Document heading
+        page_head = '''<!DOCTYPE html>
+            <html>
+             <head>
+                <meta charset="UTF-8">
+                <title>Patient Information Form</title>
+            </head>
+            <body>
                     '''
-            #This will require student information
+        # This will require student information
         page_body = '''
-                    <form method = "GET" >
-                    Name: <br><input type="text" name="name" /><br><br>
-                    Email: <br><input type="text" name="email" /><br><br>
-                    Age: <br> <input type="text" name="age" /><br><br>
-                    Sex: <br> <input type="radio" name="sex" value="male">Male <input type="radio" name="sex" value="Female"<br><br>
-                    Gpa:
-                        <br> select name="gpa">
-                          <option value="From 5.00 to 4.00">From 5.00 to 4.00</option>
-                          <option value="From 4.00 to 3.00">From 4.00 to 3.00</option>
-                          <option value="From 3.00 to 2.00">From 3.00 to 2.00</option>
-                          <option value="From 2.00 to 1.00">From 2.00 to 1.00</option>
+
+        <form method = "GET" >
+            <label>Name: </label><br><input type="text" name="name" /><br><br>
+            <label>Email: </label><br><input type="text" name="email" /><br><br>
+            <label>Age: </label><br> <input type="text" name="age" /><br><br>
+            <label>Sex: </label><br>
+                        <input type="radio" name="sex" value="male">Male
+                        <input type="radio" name="sex" value="Female">Female<br><br>
+            <label>Gpa:</label>
+            <br><select name="gpa">
+                <option value="From 5.00 to 4.00">From 5.00 to 4.00</option>
+                <option value="From 4.00 to 3.00">From 4.00 to 3.00</option>
+                <option value="From 3.00 to 2.00">From 3.00 to 2.00</option>
+                <option value="From 2.00 to 1.00">From 2.00 to 1.00</option>
                     <input type="submit" value="Submit">'''
         page_close = '''
-                     </form>
-                     </body>
-                     </html>'''
+        </form>
+            </body>
+            </html>'''
 
-        if self.request.Get:
-            #This will store info requested from the form
-            name=self.request.GET['name']
-            email=self.request.GET['email']
-            age=self.request.GET['age']
-            sex=self.request.GET['sex']
-            gpa=self.request.GET['gpa']
-            self.response.write('Name:' + name+'<br>Email: ' +email+ '<br> Age: ' +age+ '<br>Sex: ' +sex+ 'Gpa: '+gpa+'<br><br>'+page_head +page_body+page_close) #this will print output
+        if self.request.GET:
+            # This will store info requested from the form
+            name = self.request.GET['name']
+            email = self.request.GET['email']
+            age = self.request.GET['age']
+            sex = self.request.GET['sex']
+            gpa = self.request.GET['gpa']
+            self.response.write(
+                'Name:' + name + '<br>Email: ' + email + '<br> Age: ' + age + '<br>Sex: ' + sex + '<br>Gpa: ' + gpa + '<br><br>' + page_head + page_body + page_close)  # this will print output
         else:
-            self.response.write(page_head+page_body+page_close)
+            self.response.write(page_head + page_body + page_close)
 
 
-
-
-
-
-
-
-
-
-# VARIABLE THAT RETURN METHONG NEVER TOUCH IT
+# VARIABLE THAT RETURN METHOD NEVER TOUCH IT
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
 ], debug=True)
-
