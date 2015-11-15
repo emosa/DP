87bceeb73f0614a085db700f9dfca5afa123569a
Elimarie <iglitzi@fullsail.edu>
2015-11-15 18:19:41 -0500

    mainHandler
    created

--- simple-login/mainHandler.py
+++ simple-login/mainHandler.py
@@ -7,9 +7,15 @@ Reusable Library
 '''
 
 import page, lib
-c = page.connect()  # this will create object for connect class
+
+c = page.connect()  # This will create object for connect class
 while True:
     if c.data() == "POST":
-        grades = c.grades()
-        call = lib.Grade(grades[0], grades[1],grades[2], grades[3], grades[4], grades[5], grades[6], grades[7], grades[8],grades[9])
-
+        grades = c.grades()  # Obtaining data
+        cal = lib.Grade(grades[0], grades[1], grades[2], grades[3], grades[4], grades[5], grades[6], grades[7],
+                        grades[8], grades[9])  # This will send data to constructor
+        a = cal.avg()  # Get average
+        b = cal.median()  # Get median
+        _c = cal.highest()  # Get highest score
+        c.send(a, b, _c)  # Send info
+        break
