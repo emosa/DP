0576457db5216eed46114d8f82a61742b8ad25e5
Elimarie <iglitzi@fullsail.edu>
2015-11-15 19:33:21 -0500

    Contructor
    student name and values

--- Reusable Library/reusable-library/lib.py
+++ Reusable Library/reusable-library/lib.py
@@ -8,5 +8,17 @@ lib.py
 
 
 class Grade:  #  Below is the constructor ot the class to initialize data
-    def __init__(self, name1, grade1, name2, grade2, name3, name4, grade4, name5, grade5):
-        
+    def __init__(self, name1, grade1, name2, grade2, name3, grade3, name4, grade4, name5, grade5):
+        self.__name1 = name1 #  student name
+        self.__grade1 = grade1  #  grade value
+        self.__name2 = name2
+        self.__grade2 = grade2
+        self.__name3 = name3
+        self.__grade3 = grade3
+        self.__name4 = name4
+        self.__grade4 = grade4
+        self.__name5 = name5
+        self.__grade5 = grade5
+    def avg(self):
+        self
+
