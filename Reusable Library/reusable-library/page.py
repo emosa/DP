4ec9ce2030e5a8e8a48e026b7827d10fc3f6b92f
Elimarie <iglitzi@fullsail.edu>
2015-11-15 20:13:29 -0500

    socket
    adding socket settings

--- Reusable Library/reusable-library/page.py
+++ Reusable Library/reusable-library/page.py
@@ -7,5 +7,17 @@ page.py
 '''
 
 import socket
-class connect():
 
+
+class connect():
+    def __init__(self):
+        port = 8080  # this is my port
+        host = "localhost"  # host name
+        queue_size = 5
+        self.__mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.__mysocket.bind((host, port))
+        self.__mysocket.listen(queue_size)
+        self.__bool - True
+        while self.__bool:
+            print "Waiting at http://%s:%d" % (host, port)
+            (self.__connection, self.__addr) = self.__mysocket.accept()
