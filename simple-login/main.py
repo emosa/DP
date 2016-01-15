814e3d9cad5cb1351b174137abaaddc4f3dcb8fe
Elimarie <iglitzi@fullsail.edu>
2016-01-14 23:03:03 -0500

    simple
    form

--- simple-login/main.py
+++ simple-login/main.py
@@ -1,105 +1,73 @@
-# coding=utf-8
-'''
+"""
 ELIMARIE MORALES SANTIAGO
 FULL SAIL UNIVERSITY
 Design Patterns for Web Programming - Online
 Simple Form
 
-'''
+"""
 
 import webapp2  # used the webapp2 library
+from page import Page
 
 
-class MainHandler(webapp2.RequestHandler):  # declaring a class
-    def get(self):  # FUNCTION STARTS EVERYTHING
+class MainHandler(webapp2.RequestHandler):
 
-class Page(object):
-    def__init__(self):
-       self.css = 'css/styles.css'
+        def get(self):
+            page = Page()   # Creates an instance of the imported class page
 
-        # Document heading
-        self.head = '''<!DOCTYPE html>
-            <html>
-             <head>
-                <meta charset="UTF-8">
-                <title>Student Information Form</title>
-            </head>
-            <body>
+            if self.request.GET:     # Check if GET exists
+                # Get form data from url
+                name = self.request.GET['name']
+                email = self.request.GET['email']
+                university = self.request.GET['university']
+                degree = self.request.GET['degree']
+                interest = self.request.GET["interest"]
+                gpa = self.request.GET['gpa']
 
+                display = '''
+                <!DOCTYPE>
+                <html>
+                   <head>
+                      <title>Student Newsletter Confirmation Page</title>
+                      <link type="text/css" rel="stylesheet" href="css/styles.css"/>
+                   </head>
+                   <body>
+
+                 <div class="logo_wrapper">
+                 <div class="logo">
+                  <img src= "img/logo-dpwp.svg" alt="Logo"></div></div>
+
+                 <div id="nav">
+                  <ul>
+                    <li><a href="#" title="Home"><span>Home</span></a></li>
+                    <li><a href="#" title="Courses"><span>Courses</span></a></li>
+                    <li><a href="#" title="Newsletter"><span>Newsletter</span></a></li>
+                  </ul>
+                </div>
+
+                 <div class="image">
+                    <img src="img/news-1074604.jpg" alt="worldnews">
+                 </div>
+                 <div class="display">
+
+                    <h1><font size="6" color="#065F98">Newsletter Confirmation</font></h1>
+                    <p><font size="4" color="#92b749">You name is {name}, student at {university}.
+                    You have a degree on {degree}, with a average GPA of {gpa}. Your email address is: {email},
+                    and your technology of interest is {interest}. </p></font>
+                    <p><font size="4" color="#065F98">Thank you for subscribing to our newsletter.</font></p>
+                    <p> </p>
+
+                 </div>
                     '''
-        # This will require student information
-        self.body = '''
-            <div class="logo_wrapper">
-                <div class="logo">
-                    <img src= "img/logo-dpwp.svg" alt="Logo"></div></div>
 
+                display = display.format(**locals())
+                # print form content
+                self.response.write(page.header + display + page.footer)
 
-            <div id="nav">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Courses</a></li>
-                    <li><a href="#">Newsletter</a></li>
-                </ul>
-            </div>
-
-            <div class="image">
-                <img src="img/news-1074604.jpg" alt="&nbsp;">
-            </div>
-
-             <div class="header4">
-                <h1>Student Newsletter Subscription</h1>
-                <h4>If you like to receive our student newsletter, please fill out the form below.</h4>
-            </div>
-
-            <div id="section">
-
-            <form method = "GET" id="form">
-
-            <h1 class="form-title">Student Information</h1>
-            <label>Name: </label><br><input type="text" name="name" required /><br><br>
-            <label>Email: </label><br><input type="text" name="email" required /><br><br>
-            <label>Age: </label><br> <input type="text" name="age" required /><br><br>
-            <label>University: </label><br><input type="text" name="university" required /><br><br>
-            <label>Sex: </label>
-             <div class="controls">
-                 <input type="radio" name="sex" value="male">Male<br>
-                 <input type="radio" name="sex" value="Female">Female<br><br>
-
-            </div>
-            <label>Gpa:</label>
-            <br><select id="select" name="gpa">
-                <option value="From 5.00 to 4.00">From 5.00 to 4.00</option>
-                <option value="From 4.00 to 3.00">From 4.00 to 3.00</option>
-                <option value="From 3.00 to 2.00">From 3.00 to 2.00</option>
-                <option value="From 2.00 to 1.00">From 2.00 to 1.00</option><br><br>
-                <input class="button" type="submit" value="Submit">
-                '''
-
-        page_close = '''
-        </form>
-            </div>
-                <div id="footer">
-                    Copyright © Elimarie Morales Santiago
-            </div>
-        </body>
-        </html>'''
-
-        if self.request.GET:
-            # This will store info requested from the form
-            name = self.request.GET['name']
-            email = self.request.GET['email']
-            age = self.request.GET['age']
-            sex = self.request.GET['sex']
-            university = self.request.GET["university"]
-            gpa = self.request.GET['gpa']
-            self.response.write(
-                'Name:' + name + '<br>Email: ' + email + '<br>Age: ' + age + '<br>Sex: ' + sex + '<br>University:' + university + '<br>Gpa: ' + gpa + '<br><br>' + page_head + page_close)  # this will print output
-        else:
-            self.response.write(page_head + page_body + page_close)
-
+            else:
+                self.response.write(page.header + page.body + page.footer)
 
 # VARIABLE THAT RETURN METHOD NEVER TOUCH IT
-
-app = webapp2.WSGIApplication([
+app = webapp2.WSGIApplication({
     ('/', MainHandler)
-], debug=True)
+}, debug=True)
